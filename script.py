446213390b2d2afcdac275a56c5cf8c043cc2871
Vali <valibojici@gmail.com>
2021-08-21 19:25:59 +0300

    update

--- script.py
+++ script.py
@@ -1,7 +1,14 @@
+#!/usr/bin/python
+
 import json
 
 with open('test.txt', 'r') as f:
 	content = f.read()
+	chars = {'<' : '&lt;', '>' : '&gt;', '"' : '$quot;', "'" : '&#039;', '&' : '&amp;'}
+	
+	for k,v in chars.items():
+		content = content.replace(k, v)
+
 	jsonData = json.dumps({'content' : content})
 	
 	with open('output.json', 'w') as g:
