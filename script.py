8a5963029dc4775782b89cd887c2caea5438b98e
Vali <valibojici@gmail.com>
2021-08-21 19:49:58 +0300

    upoda

--- script.py
+++ script.py
@@ -4,10 +4,6 @@ import json
 
 with open('test.txt', 'r') as f:
 	content = f.read()
-	chars = {'<' : '&lt;', '>' : '&gt;', '"' : '$quot;', "'" : '&#039;', '&' : '&amp;'}
-	
-	for k,v in chars.items():
-		content = content.replace(k, v)
 
 	solution = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita exercitationem culpa, corporis delectus quia quas placeat ipsam nobis aut ratione natus modi? Voluptas nobis harum unde modi aspernatur? Accusamus iste ipsam, atque non corporis odit obcaecati, labore molestias repellendus hic rem dolorem velit optio incidunt! Et, numquam libero nemo at placeat officia soluta corrupti aliquam animi ea magni ipsa quam expedita cumque perferendis atque recusandae ullam eum amet! Vel ipsam facere eum minima, officiis sunt doloremque totam mollitia aliquam non inventore saepe eos voluptate ratione. Dolores earum maxime labore nemo nobis dolorem porro deserunt omnis magni mollitia. Adipisci, maxime eum.<pre><code>int x = 5; </code></pre> recusandae ullam eum amet! Vel ipsam facere eum minima, officiis sunt doloremque totam mollitia aliquam non inventore saep"
 
